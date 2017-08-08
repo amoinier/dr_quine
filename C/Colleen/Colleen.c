ca23cbe0a7fcd58b5f6d1f40d4b1ecf5696442a9
Alex MOINIER <amoinier@student.42.fr>
2017-08-08 14:33:34 +0200

    fix error + create makefile

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,12 +1,13 @@
 /*
-FT_PRINTMYNAME
+	FT_PRINTMYNAME
 */
+#include <stdio.h>
 char *ft_printmyname(void) {return("Alex");}
 int main(void) {
 /*
-str to write in printf
+	str to write in printf
 */
-	char *str = "/*%cFT_PRINTMYNAME%c*/%cchar *ft_printmyname(void) {return(%cAlex%c);}%cint main(void) {%c/*%cstr to write in printf%c*/%c%cchar *str = %c%s%c;%c%cprintf(str, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 10);%c%cft_printmyname();%c}%c";
-	printf(str, 10, 10, 10, 34, 34, 10, 10, 10, 10, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 10);
+	char *str = "/*%c%cFT_PRINTMYNAME%c*/%c#include <stdio.h>%cchar *ft_printmyname(void) {return(%cAlex%c);}%cint main(void) {%c/*%c%cstr to write in printf%c*/%c%cchar *str = %c%s%c;%c%cprintf(str, 10, 9, 10, 10, 10, 34, 34, 10, 10, 10, 9, 10, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 10);%c%cft_printmyname();%c}%c";
+	printf(str, 10, 9, 10, 10, 10, 34, 34, 10, 10, 10, 9, 10, 10, 9, 34, str, 34, 10, 9, 10, 9, 10, 10);
 	ft_printmyname();
 }
