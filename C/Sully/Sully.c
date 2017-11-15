cbe8e4cc2d1a4baad099c9590d4ddb025087a273
amoinier <amoinier@student.42.fr>
2017-11-15 13:24:21 +0100

    fix Sully

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -2,4 +2,25 @@ int i = 5;
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main(void) { if (i >= 0) { if (strchr(__FILE__, i + 48)) { i--; } char c = (i + 48); char *str = strcat(strdup(strcat(strdup("Sully_"), &c)), ".c"); char *str2 = strdup(str); str2[7] = '\0'; FILE *fp = fopen(str, "w"); if (fp) { char *quine = "int i = %d;%c#include <stdio.h>%c#include <string.h>%c#include <stdlib.h>%cint main(void) { if (i >= 0) { if (strchr(__FILE__, i + 48)) { i--; } char c = (i + 48); char *str = strcat(strdup(strcat(strdup(%cSully_%c), &c)), %c.c%c); char *str2 = strdup(str); str2[7] = %c%c0%c; FILE *fp = fopen(str, %cw%c); if (fp) { char *quine = %c%s%c; fprintf(fp, quine, i, 10, 10, 10, 10, 34, 34, 34, 34, 39, 92, 39, 34, 34, 34, quine, 34, 34, 34, 34, 34, 34, 34, 10); fclose(fp); system(strcat(strdup(strcat(strdup(strcat(strdup(%cclang -Wall -Wextra -Werror %c), str)), %c -o %c)), str2)); } if (i > 0) { system(strcat(strdup(%c./%c), str2)); }} return 0;}%c"; fprintf(fp, quine, i, 10, 10, 10, 10, 34, 34, 34, 34, 39, 92, 39, 34, 34, 34, quine, 34, 34, 34, 34, 34, 34, 34, 10); fclose(fp); system(strcat(strdup(strcat(strdup(strcat(strdup("clang -Wall -Wextra -Werror "), str)), " -o ")), str2)); } if (i > 0) { system(strcat(strdup("./"), str2)); }} return 0;}
+int main(void) {
+if (i >= 0) {
+if (!strstr(__FILE__, "Sully.c")) {
+i--;
+}
+char str[70];
+sprintf(str, "Sully_%1$d.c", i);
+FILE *fp = fopen(str, "w");
+if (fp) {
+char *quine = "int i = %4$d;%1$c#include <stdio.h>%1$c#include <string.h>%1$c#include <stdlib.h>%1$cint main(void) {%1$cif (i >= 0) {%1$cif (!strstr(__FILE__, %2$cSully.c%2$c)) {%1$ci--;%1$c}%1$cchar str[70];%1$csprintf(str, %2$cSully_%5$c1$d.c%2$c, i);%1$cFILE *fp = fopen(str, %2$cw%2$c);%1$cif (fp) {%1$cchar *quine = %2$c%3$s%2$c;%1$cfprintf(fp, quine, 10, 34, quine, i, 37);%1$cfclose(fp);%1$csprintf(str, %2$cgcc Sully_%5$c1$d.c -o Sully_%5$c1$d%2$c, i);%1$csystem(str);%1$c}%1$cif (i > 0) {%1$csprintf(str, %2$c./Sully_%5$c1$d%2$c, i);%1$csystem(str);%1$c}%1$c}%1$creturn 0;%1$c}%1$c";
+fprintf(fp, quine, 10, 34, quine, i, 37);
+fclose(fp);
+sprintf(str, "gcc Sully_%1$d.c -o Sully_%1$d", i);
+system(str);
+}
+if (i > 0) {
+sprintf(str, "./Sully_%1$d", i);
+system(str);
+}
+}
+return 0;
+}
